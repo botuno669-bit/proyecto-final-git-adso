0ebb071d56be11a664061848eb2241742e264619
botuno <botuno669@gmail.com>
2025-11-24 14:04:38 -0500

    Agregar estructura base de la rama Login

---
+++ app.js
@@ -0,0 +1,10 @@
+Rama: feature/login   
+Fecha: 24/11/2025
+Descripcion:
+
+let contador = 0;
+
+function aumentarContador() {
+  contador++;
+  document.getElementById("resultado").textContent = "Contador: " + contador;
+}
